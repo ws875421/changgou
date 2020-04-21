f90276af9dcf524221a0763f37df8eaff156d228
Rex <ws0936167890@gmail.com>
2020-04-21 17:13:30 +0800

    ExceptionHandler

---
+++ changgou-service/changgou-service-goods/src/main/java/com/changgou/controller/BaseExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.changgou.controller;
+
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class BaseExceptionHandler {
+
+    /***
+     * 异常处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public Result error(Exception e) {
+        e.printStackTrace();
+        return new Result(false, StatusCode.ERROR, e.getMessage());
+    }
+}
